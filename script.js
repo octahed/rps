557948009672678a9d72fe4b44507ee839b29ac0
octahed <142986153+octahed@users.noreply.github.com>
2023-09-13 12:57:53 +0300

    Add function that plays a round of the game

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function getPlayerChoice() {
 		"Enter a number that corresponds with your choice:");
 	return choices[playerChoice - 1];
 }
+
+// Function that plays a round of the game
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection === computerSelection) {
+		return "It's a tie!";
+	} else if (playerSelection === 'rock' && computerSelection === 'paper') {
+		return "You lose! Paper beats Rock";
+	} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+		return "You win! Rock beats Scissors";
+	} else if (playerSelection === 'paper' && computerSelection === 'rock') {
+		return "You win! Paper beats Rock";
+	} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+		return "You lose! Scissors beat Paper";
+	} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+		return "You lose! Rock beats Scissors";
+	} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+		return "You win! Scissors beat Paper";
+	}
+}
