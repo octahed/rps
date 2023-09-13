66bebdd85ef3eeff709f407d9ddb23a8b955d647
octahed <142986153+octahed@users.noreply.github.com>
2023-09-13 12:48:48 +0300

    Add function that gets a choice from the player

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
 	return choices[Math.floor(Math.random() * 3)]
 }
+
+function getPlayerChoice() {
+	let playerChoice = prompt("1.Rock 2.Paper 3.Scissors\n" +
+		"Enter a number that corresponds with your choice:");
+	return choices[playerChoice - 1];
+}
